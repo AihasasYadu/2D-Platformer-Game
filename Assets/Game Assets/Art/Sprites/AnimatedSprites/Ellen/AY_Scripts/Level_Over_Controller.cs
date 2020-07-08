c0d9139504471fb4d2ccd9c97f8753594bfc20f4
AihasasYadu <AihasasYadu@users.noreply.github.com>
2020-07-09 03:32:35 +0530

    Jump animation cropped down to 1 for both Jumps Added Ground Triggered Crouch Animation Added Next Scene along with a Next Level Trigger Added Platform using TileMaps Added a Level Over Controller Script

---
+++ Assets/Game Assets/Art/Sprites/AnimatedSprites/Ellen/AY_Scripts/Level_Over_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Over_Controller : MonoBehaviour
+{
+    public string nextScene;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collided With:" + collision.gameObject.name);
+        if (collision.gameObject.GetComponent<Ellen_Movement>() != null)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
